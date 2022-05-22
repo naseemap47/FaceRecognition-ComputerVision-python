f7b4bf4ef5f48b38017302044beb98839ad0ea49
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-22 11:39:10 +0530

    added utils file, write function to find encodings of train dataset

---
+++ my_utils.py
@@ -0,0 +1,11 @@
+import cv2
+import face_recognition as fr
+
+
+def findEncodings(images):
+    face_encodings = []
+    for img in images:
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        face_encod = fr.face_encodings(img_rgb)
+        face_encodings.append(face_encod)
+    return face_encodings
