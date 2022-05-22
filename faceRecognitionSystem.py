f5c00c684ff0325f929a54ffac28b2c78940d4dd
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-22 15:22:16 +0530

    fixed typo error in for loop in 39th line, if any maches happens, it will draw rectangle and show name on top of it

--- faceRecognitionSystem.py
+++ faceRecognitionSystem.py
@@ -15,8 +15,9 @@ test_path = 'test'
 classNames = []
 images = []
 myList = os.listdir(train_path)
+myList.sort()  # Sort by alphabetical Order
 for cls in myList:
-    classNames.append(cls)
+    classNames.append(os.path.splitext(cls)[0])
     img = cv2.imread(f'{train_path}/{cls}')
     img = cv2.resize(img, (width, height))
     images.append(img)
@@ -30,15 +31,30 @@ cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
-    imgS = cv2.resize(img, (width, height))  # Resize
-    img_rgb = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)  # BGR to RGB
+    # imgS = cv2.resize(img, (width, height))  # Resize
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # BGR to RGB
     face_locations = fr.face_locations(img_rgb)
     face_encodes = fr.face_encodings(img_rgb, face_locations)
 
-    for encode, loc in zip(train_encodings, face_locations):
+    for encode, loc in zip(face_encodes, face_locations):
         matches = fr.compare_faces(train_encodings, encode)
         face_dist = fr.face_distance(train_encodings, encode)
         print(face_dist)
+        match_index = np.argmin(face_dist)
+
+        if matches[match_index]:
+            name = classNames[match_index]
+            print(name)
+            # Draw
+            cv2.putText(
+                img, f'{name}', (loc[3], loc[0]-6),
+                cv2.FONT_HERSHEY_PLAIN, 1.5,
+                (255, 255, 0), 2
+            )
+            cv2.rectangle(
+                img, (loc[3], loc[0]), (loc[1], loc[2]),
+                (0, 255, 0), 2
+            )
 
     cv2.imshow('Web-cam', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
