4dd5b934f15fe4670c7f8d3c535207860c5eaeab
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-22 13:17:02 +0530

    converted webcam feed into rgb, resized, find encoding for webcam feed, compared with train encodings, printed face distance

--- faceRecognitionSystem.py
+++ faceRecognitionSystem.py
@@ -1,6 +1,8 @@
 import cv2
 import os
 from my_utils import findEncodings
+import face_recognition as fr
+import numpy as np
 
 ############################################
 # Parameters
@@ -21,13 +23,22 @@ for cls in myList:
 # print(classNames)
 
 # Train Encodings
-encodings = findEncodings(images)
+train_encodings = findEncodings(images)
 
 # Webcam
 cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
+    imgS = cv2.resize(img, (width, height))  # Resize
+    img_rgb = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)  # BGR to RGB
+    face_locations = fr.face_locations(img_rgb)
+    face_encodes = fr.face_encodings(img_rgb, face_locations)
+
+    for encode, loc in zip(train_encodings, face_locations):
+        matches = fr.compare_faces(train_encodings, encode)
+        face_dist = fr.face_distance(train_encodings, encode)
+        print(face_dist)
 
     cv2.imshow('Web-cam', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
