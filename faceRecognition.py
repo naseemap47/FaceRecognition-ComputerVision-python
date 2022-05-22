7f50c01172229e522acdfe6e28f8238f134f6463
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-22 10:46:13 +0530

    draw rectangle on faces

--- faceRecognition.py
+++ faceRecognition.py
@@ -13,7 +13,14 @@ img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
 faceLoc = fr.face_locations(img1)
 # print(faceLoc)
 encode_img1 = fr.face_encodings(img1)
-print(encode_img1)
+# print(encode_img1)
+
+# Draw Rectangle
+for i in faceLoc:
+    # y, x+w, y+h, x = faceLoc
+    cv2.rectangle(img1, (i[3], i[0]), (i[1], i[2]),
+                  (0, 255, 0), 2
+                  )
 
 # Test
 img1T = fr.load_image_file('test/Darshan.jpg')
@@ -23,7 +30,15 @@ img1T = cv2.cvtColor(img1T, cv2.COLOR_BGR2RGB)
 faceLocT = fr.face_locations(img1T)
 # print(faceLocT)
 encode_img1T = fr.face_encodings(img1T)
-print(encode_img1T)
+# print(encode_img1T)
+
+# Draw Rectangle
+for i in faceLocT:
+    # y, x+w, y+h, x = faceLoc
+    cv2.rectangle(img1T, (i[3], i[0]), (i[1], i[2]),
+                  (0, 255, 0), 2
+                  )
+
 
 cv2.imshow('Train', img1)
 cv2.imshow('Test', img1T)
