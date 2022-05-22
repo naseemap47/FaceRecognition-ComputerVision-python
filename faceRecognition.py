6e2d9d805545193b289c68518d00526dd3213ddc
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-22 10:34:24 +0530

    loaded train and test img, resized and convert into RBG and find locations of faces

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,3 +1,27 @@
 import cv2
 import face_recognition as fr
 
+# Parameters
+width = 640
+height = 640
+
+# Train
+img1 = fr.load_image_file('train/Darshan.jpg')
+img1 = cv2.resize(img1, (width, height))
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+
+faceLoc = fr.face_locations(img1)
+print(faceLoc)
+
+# Test
+img1T = fr.load_image_file('test/Darshan.jpg')
+img1T = cv2.resize(img1T, (width, height))
+img1T = cv2.cvtColor(img1T, cv2.COLOR_BGR2RGB)
+
+faceLocT = fr.face_locations(img1T)
+print(faceLocT)
+
+cv2.imshow('Train', img1)
+cv2.imshow('Test', img1T)
+if cv2.waitKey(0) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
