7d605a7473ee2d4cc71c0f4df367c9458f4f8278
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-22 13:14:03 +0530

    Updated findEncodings function, train data contain only single person img, so taking first element (only contain one element)

--- my_utils.py
+++ my_utils.py
@@ -6,6 +6,6 @@ def findEncodings(images):
     face_encodings = []
     for img in images:
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        face_encod = fr.face_encodings(img_rgb)
+        face_encod = fr.face_encodings(img_rgb)[0]
         face_encodings.append(face_encod)
     return face_encodings
