ae4626ef980337552167015866ed67ad9cdf09ea
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-23 16:06:28 +0530

    added getAttendance function to save attendance data in a CSV file

--- my_utils.py
+++ my_utils.py
@@ -1,5 +1,6 @@
 import cv2
 import face_recognition as fr
+import datetime
 
 
 def findEncodings(images):
@@ -9,3 +10,18 @@ def findEncodings(images):
         face_encod = fr.face_encodings(img_rgb)[0]
         face_encodings.append(face_encod)
     return face_encodings
+
+
+def getAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        data = f.readlines()
+        name_list = []
+        for line in data:
+            entry = line.split(',')
+            name_list.append(entry[0])
+
+        # if name NOT present in the list, it will add
+        if name not in name_list:
+            c_time = datetime.datetime.now()
+            date_str = c_time.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{date_str}')
